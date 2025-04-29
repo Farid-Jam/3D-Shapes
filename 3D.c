32924702dc72c17fd0abf77764cda14f93aa51ed
Farid-Jam <farid.jamshid77@gmail.com>
2025-04-28 20:06:22 -0400

    Fixed culling

--- 3D.c
+++ 3D.c
@@ -8,7 +8,7 @@ int windowHeight = 700;
 int running = 1;
 SDL_Event event;
 int numFaces = 6;
-int culling = 0; // 0 to turn off, any other number to turn on
+int culling = 1; // 0 to turn off, any other number to turn on
 
 // Structure to store 3-dimensional points
 typedef struct point3D
@@ -28,16 +28,44 @@ typedef struct point2D
 // Structure to store a face of a cube
 typedef struct face
 {
-    struct point3D a, b, c, d;
+    point3D a, b, c, d;
+    point3D normal;
+    double dot;
 } face;
 
 // Method to determine whether the normal of a face points towards the camera 
-double backfaceCulling(face face)
+point3D findNormal(face face)
 {
     point3D edge1 = {face.d.x - face.a.x, face.d.y - face.a.y, face.d.z - face.a.z};
     point3D edge2 = {face.b.x - face.a.x, face.b.y - face.a.y, face.b.z - face.a.z};
 
-    return ((edge1.x * edge2.y) - (edge1.y * edge2.x) < 0);
+    point3D normal = {
+        (edge1.y * edge2.z) - (edge1.z * edge2.y),
+        (edge1.z * edge2.x) - (edge1.x * edge2.z),
+        (edge1.x * edge2.y) - (edge1.y * edge2.x)
+    };
+
+    return normal;
+}
+
+// Method to determine how much a faces normal vector differs from a vector from the camera to the center of a face
+double findDot(face face, point3D camPos)
+{
+    point3D center = {
+        ((face.a.x + face.b.x) / 2 + face.d.x / 2),
+        ((face.a.y + face.b.y) / 2 + face.d.y / 2), 
+        ((face.a.z + face.b.z) / 2 + face.d.z / 2)
+    };
+
+    point3D vector = {
+        center.x - camPos.x,
+        center.y - camPos.y,
+        center.z - camPos.z
+    };
+
+    double dot = vector.x * face.normal.x + vector.y * face.normal.y + vector.z * face.normal.z;
+
+    return dot;
 }
 
 // Method to projecta 3-demensional point onto a 2-dimensional plane, returning the 2-dimensional point
@@ -87,6 +115,9 @@ int main()
 {
     // Initialize system
     SDL_Init(SDL_INIT_VIDEO);
+
+    // Initialize camera position
+    point3D camera = {0, 0, 0};
 
     // Create window
     SDL_Window *window = SDL_CreateWindow("Cube3D", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
@@ -157,11 +188,12 @@ int main()
 
             }
         }
+        
         // Prepare for next frame by clearing previous render
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        // Draw lines connecting points in every face 
+        // Draw lines connecting points in every visible face
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         for (int i = 0; i < numFaces; i++)
         {
@@ -169,7 +201,9 @@ int main()
             point2D v2 = project(faces[i].b);
             point2D v3 = project(faces[i].c);
             point2D v4 = project(faces[i].d);
-            if (backfaceCulling(faces[i]) || !culling ){
+            faces[i].normal = findNormal(faces[i]);
+            faces[i].dot = findDot(faces[i], camera);
+            if (faces[i].dot < 0|| !culling ){
                 SDL_RenderDrawLine(renderer, v1.x, v1.y, v2.x, v2.y);
                 SDL_RenderDrawLine(renderer, v2.x, v2.y, v3.x, v3.y);
                 SDL_RenderDrawLine(renderer, v3.x, v3.y, v4.x, v4.y);
